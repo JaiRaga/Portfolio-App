809b3a17ccbb68aea237bfffca763406eff91656
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-06 20:09:56 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Drawer from "./component/Drawer";
+import Home from "./component/Home";
+import Projects from "./component/Projects";
+import About from "./component/About";
+import Certifications from "./component/Certifications";
+import Resume from "./component/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Drawer />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/project' component={Projects} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/certificates' component={Certifications} />
+          <Route exact path='/contact' component={Resume} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
