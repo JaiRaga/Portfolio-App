5ce9f4dfb18a316c2f890b2568345514ffa2ea68
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-06 20:08:09 +0530

    modified css

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Grid, Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    minHeight: "90vh"
+  }
+}));
 
 const Home = () => {
-  return <div>Home</div>;
+  const classes = useStyles();
+  return (
+    <Grid container justify='center' className={classes.container}>
+      <Grid
+        container
+        item
+        direction='column'
+        justify='center'
+        alignItems='center'
+        xs={8}
+        md={10}
+        lg={8}>
+        <Grid item>
+          <Typography variant='h1'>Hello</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant='h6' align='center'>
+            I am Raga! I Code and Build Web Apps...
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Home;
